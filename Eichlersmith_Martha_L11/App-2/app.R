dd3d58f6d2e9b2b3254d6048f9a7ba3d5c07a9b2
MarEichler <martha.eichlersmith@gmail.com>
2020-05-18 12:14:28 -0500

    add app 2 for lesson 2

---
+++ Eichlersmith_Martha_L11/App-2/app.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(tidyverse)
+
+
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("Lesson 2"),
+  
+  sidebarLayout(position = "left",
+                sidebarPanel("Check out my Guinea Pigs"),
+                mainPanel(
+                  h1("Daffodil and Bloosom", align = "center"),
+                  p("These are my two guinea pigs.  I adopted them in January 2019.", style = "font-family: 'times'; font-si16pt"), 
+                  strong("They are are very cute."),
+                  br(),
+                  code("They have no idea how to code."),
+                  div("They do know how to run around the cage and wheek for food.", style = "color:blue"),
+                  br(),
+                  p("Their favorite foods are",
+                    span("cilantro, parsely,", style = "color:green"),
+                    "and", 
+                    span("carrots", style = "color:orange"), 
+                    "."), 
+                  img(src = "peegs.jpg", width = "80%")
+                )
+  )
+)
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
