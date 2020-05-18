2d4106a6256086bf4729551257ad7a26cc7b30b0
MarEichler <martha.eichlersmith@gmail.com>
2020-05-18 15:24:57 -0500

    update exercise 3

--- Eichlersmith_Martha_L11/MN_map/app.R
+++ Eichlersmith_Martha_L11/MN_map/app.R
@@ -1,10 +1,33 @@
 #MINNESOTA MAP 
 library(shiny)
 library(tidyverse)
+library(rvest) #for scraping data from mn web 
 library(maps)
+
 
 mn <- map_data("county", "minnesota") %>% 
   select(long, lat, group, id = subregion)
+
+
+url <- "https://www.minnesota-demographics.com/counties_by_population"
+webpage <- read_html(url)
+tbls <- html_nodes(webpage, "table")
+
+tbls_ls <- webpage %>%
+  html_nodes("table") %>%
+  html_table()
+
+pop_mn_counties <- tbls_ls[[1]][-88, ] %>%
+  mutate(
+    Population = as.numeric(gsub(",", "",  Population))
+    , County = tolower(gsub(" County", "", County))
+    , County = ifelse(County == "st. louis", "st louis", County)
+    , perc_pop = Population / sum(Population)
+  ) %>%
+  janitor::clean_names() 
+
+mn_pop_map <- mn %>%
+  left_join(., pop_mn_counties, by = c("id" = "county"))
 
 # Define UI ----
 ui <- fluidPage(
@@ -39,10 +62,15 @@ ui <- fluidPage(
 # Define server logic ----
 server <- function(input, output) {
   output$statemap <-  renderPlot({
-  ggplot(mn, aes(x = long, y = lat)) +
-    geom_polygon(aes(group = group), fill = "#ADD8E6" , color = "grey35") +
+    ggplot(mn_pop_map, aes(x = long, y = lat, fill = perc_pop)) +
+      geom_polygon(aes(group = group) , color = "grey35") +
+      scale_fill_continuous(
+        name = "Percent of Population"
+        , breaks = c(0.05, 0.10, 0.15, 0.20)
+        , labels = c("5%", "10%", "15%", "20%")
+      ) +
     coord_quickmap() +
-    theme_void()
+      theme_void()
   })
   
 }
