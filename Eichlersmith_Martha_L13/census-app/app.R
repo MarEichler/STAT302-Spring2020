2574cf72a3a45d7f1f4048fdcbd5e9c9245a0670
MarEichler <martha.eichlersmith@gmail.com>
2020-05-27 11:20:11 -0500

    individual 48 states

---
+++ Eichlersmith_Martha_L13/census-app/app.R
@@ -0,0 +1,69 @@
+#lesson 5
+library(maps)
+library(mapproj)
+library(tidyverse)
+source("helpers.R")
+source("func_statemap.R")
+
+counties <- readRDS("data/counties.rds")  %>%
+    separate(name, c("state", "county"), ",")
+
+counties_map <- map_data("county") %>% 
+    select(long, lat, group, county = subregion, state = region) %>% 
+    left_join(., counties, by = c("county" = "county",  "state" = "state"))
+
+
+# User interface ----
+ui <- fluidPage(
+    titlePanel("censusVis"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+            
+            selectInput("state", label = "Select a state or the contiguous 48 states to display", 
+                        choices = state.name, selected = "South Dakota"),
+            
+            selectInput("var", 
+                        label = "Choose a variable to display",
+                        choices = c("Percent White", "Percent Black",
+                                    "Percent Hispanic", "Percent Asian"),
+                        selected = "Percent White"),
+            
+            sliderInput("range", 
+                        label = "Range of interest:",
+                        min = 0, max = 100, value = c(0, 100))
+        ),
+        
+        mainPanel(plotOutput("map"))
+    )
+)
+
+# Server logic ----
+server <- function(input, output) {
+    
+    dataInput <- reactive({
+        counties_map %>%
+            filter(state == tolower(input$state))
+    })
+    
+    output$map <- renderPlot({
+        args <- switch(input$var,
+                       "Percent White" = list(dataInput()$white, "darkgreen", "% White"),
+                       "Percent Black" = list(dataInput()$black, "dodgerblue", "% Black"),
+                       "Percent Hispanic" = list(dataInput()$hispanic, "darkorange", "% Hispanic"),
+                       "Percent Asian" = list(dataInput()$asian, "darkviolet", "% Asian"))
+        
+        args$perc_min <- input$range[1]
+        args$perc_max <- input$range[2]
+        args$map_data <- dataInput() 
+        args$state_name <- input$state
+        
+        
+        do.call(state_map, args)
+    })
+}
+
+# Run app ----
+shinyApp(ui, server)
---
+++ Eichlersmith_Martha_L13/census-app/func_statemap.R
@@ -0,0 +1,30 @@
+state_map <- function(map_data, fill_var, fill_color, legend_name, perc_min, perc_max, state_name){
+
+  perc_breaks <- seq(perc_min, perc_max, (perc_max-perc_min)/4)
+  perc_labels <- paste(perc_breaks, "%", sep = "")
+  if(perc_max < 100){perc_labels[5] <- paste(perc_labels[5], "or more")}
+  
+  
+  ggplot(map_data, aes(x = long, y = lat)) +
+    geom_polygon(aes(group = group, fill = fill_var)) +
+    ggtitle(state_name) + 
+    borders("state", state_name, colour = "black") +
+    scale_fill_gradient(
+        name = legend_name
+      , high = fill_color
+      , low = "white"
+      , na.value = fill_color
+      , limits = c(perc_min, perc_max)
+      , breaks = perc_breaks 
+      , labels = perc_labels 
+    ) +
+    coord_quickmap() +
+    theme_void() +
+    theme(
+        plot.title = element_text(size = 24, hjust = 0.5)
+      , legend.title = element_text(size = 16)
+      , legend.text = element_text(size = 12)
+    )
+}
+
+
